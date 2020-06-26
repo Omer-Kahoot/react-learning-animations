7b4e4f87ec6631089c2c396fbb49205410a6e227
Omer Kahoot <omer@mauqa.online>
2020-06-26 17:19:46 +0500

    instead of defining classes manually using css transition component to get the classes assigned automatically. the transition group library that we are using basically provides from classes fade-slide-enter, fade-slide-enter-active, fade-slide-exit, fade-slide-exit-active. Without active ones that we can use for initialization and with active ones where the animation will be played

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Transition from 'react-transition-group/Transition';
+//import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
@@ -14,7 +15,7 @@ const modal = (props) => {
         props.show === 'Entering' ? 'ModalOpen' :
             props.show === 'Exiting' ? 'ModalClosed' : null];*/
     return (
-        <Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
+        /*<Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
             {state => {
                 const cssClasses = ['Modal',
                     state === 'entering' ? 'ModalOpen' :
@@ -27,7 +28,18 @@ const modal = (props) => {
                     </div>
                 );
             }}
-        </Transition>
+        </Transition>*/
+
+        <CSSTransition  mountOnEnter 
+                        unmountOnExit 
+                        in={props.show} 
+                        timeout={animationTiming}
+                        classNames="fade-slide">
+            <div className="Modal">
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+        </CSSTransition>
     );
 };
 
